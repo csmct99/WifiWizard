ddd8e33117bd321f06ad85bd81b2071b3c0b3e07
Holly McFarland <me@hollymcfarland.com>
2021-03-23 21:09:49 -0400

    track total connectivity in a UI element

--- Wifi Wizard/Assets/Scripts/CheckConnectivity.cs
+++ Wifi Wizard/Assets/Scripts/CheckConnectivity.cs
@@ -6,6 +6,21 @@ public class CheckConnectivity : MonoBehaviour
 {
     public Material noConnection;
     public Material connection;
+
+    public bool isConnected()
+    {
+        foreach (GameObject wifi in GameObject.FindGameObjectsWithTag("Wifi"))
+        {
+            Collider c = wifi.GetComponent<Collider>();
+            Vector3 closest = c.ClosestPoint(transform.position);
+            if ((closest - transform.position).sqrMagnitude < Mathf.Epsilon * Mathf.Epsilon)
+            {
+                gameObject.GetComponent<Renderer>().material = connection;
+                return true;
+            }
+        }
+        return false;
+    }
     // Start is called before the first frame update
     void Start()
     {
@@ -15,16 +30,6 @@ public class CheckConnectivity : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        foreach (GameObject wifi in GameObject.FindGameObjectsWithTag("Wifi"))
-        {
-            Collider c = wifi.GetComponent<Collider>();
-            Vector3 closest = c.ClosestPoint(transform.position);
-            if ((closest - transform.position).sqrMagnitude < Mathf.Epsilon * Mathf.Epsilon)
-            {
-                gameObject.GetComponent<Renderer>().material = connection;
-                return;
-            }
-        }
-        gameObject.GetComponent<Renderer>().material = noConnection;
+        gameObject.GetComponent<Renderer>().material = isConnected() ? connection : noConnection;
     }
 }
